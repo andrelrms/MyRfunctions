f9401b51e744570a47b7b6abc64aceeadc012d1d
Ande Ramos <andre_rms@hotmail.com>
2020-03-18 14:17:57 -0700

    First Commit

---
+++ R/funcoes.R
@@ -0,0 +1,31 @@
+library(tidyverse)
+library(readxl)
+library(brazilmaps)
+library(gridExtra)
+
+centroid <- function(x, pos = 'both'){
+  unlist_geom <- unlist(x)
+  n_elem <- length(unlist_geom)
+  x_coord <- median(unlist_geom[1:(n_elem/2)])
+  y_coord <- median(unlist_geom[(n_elem/2 + 1):n_elem])
+  ret_coord <- c(x_coord,y_coord)
+  if(pos=='x'){
+    ret_coord <- x_coord
+  }
+  if(pos=='y'){
+    ret_coord <- y_coord
+  }
+  
+  return(ret_coord)
+}
+
+
+addUnits <- function(n) {
+  labels <- ifelse(n < 1000, n,  # less than thousands
+                   ifelse(n < 1e6, paste0(round(n/1e3), 'k'),  # in thousands
+                          ifelse(n < 1e9, paste0(round(n/1e6), 'M'),  # in millions
+                                 ifelse(n < 1e12, paste0(round(n/1e9), 'B'), # in billions
+                                        'too big!'
+                                 ))))
+  return(labels)
+}
